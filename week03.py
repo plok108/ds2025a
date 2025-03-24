7cca529e0883fcda9cb81100c76dd203aaae6c51
plok108 <plok108@naver.com>
2025-03-24 17:22:45 +0900

    v0.6 array

--- week03.py
+++ week03.py
@@ -1,8 +1,27 @@
-import array
+def move_zeros(l):
+    zero_idx = 0;
+    for i in range(len(l)):
+        n = l[i]
+        if n != 0:
+            l[zero_idx] = n
+            if zero_idx != i:
+                l[i] = 0
+            zero_idx = zero_idx + 1
+    return l
 
-arr = array.array('f',[99, 0, -7, 0, 16])
 
-for i in range(len(arr)):
-    print(f"{arr[i]:3}, {id(arr[i])}")
+l = [99, 0, -7, 0, 16]
+move_zeros(l)
+print(l)
+
+
+
+#import array
+#
+#arr = array.array('f',[99, 0, -7, 0, 16])
+#
+#for i in range(len(arr)):
+#    print(f"{arr[i]:3}, {id(arr[i])}")
+
 
 
