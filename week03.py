c4b8ae293b48e4fd7df375f65454c1d5c61fc09f
plok108 <plok108@naver.com>
2025-03-24 18:06:40 +0900

    v1.0 array set = dict3

--- week03.py
+++ week03.py
@@ -1,21 +1,10 @@
-def duplicate_city(cities):
-    result_city = []
-    s = set()
+def inters(l1, l2):
+    l3 = []
+    for v in l1:
+        if v in l2:
+            l3.append(v)
+    return l3
 
-    for city in cities:
-        l1 = len(s)
-        s.add(city)
-        l2 = len(s)
-        if l1 == l2:
-            result_city.append(city)
-    return result_city
-
-cities = ['Incheon', 'Seoul', 'Incheon', 'Incheon', 'Gwangju']
-#cities = set(cities)
-#cities.add('Incheon')
-#cities.add('Suwon')
-cities.append('Incheon')
-cities.append('Seoul')
-cities.append('Suwon')
-print(cities)
-print(set(duplicate_city(cities)))  # 중복값을 찾아서 하나만 나오도록 하는 방법
+l1 = [45, 5, 22 ,31, 7, 19]
+l2 = [22, 1, 5, 2, 7, 28, 27, 19, 13, 41]
+print(inters(l1, l2))
