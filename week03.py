01fb948f96f0823d54e111dc28214adf63605854
plok108 <plok108@naver.com>
2025-03-24 17:39:29 +0900

    v0.7 array zip

--- week03.py
+++ week03.py
@@ -1,27 +1,6 @@
-def move_zeros(l):
-    zero_idx = 0;
-    for i in range(len(l)):
-        n = l[i]
-        if n != 0:
-            l[zero_idx] = n
-            if zero_idx != i:
-                l[i] = 0
-            zero_idx = zero_idx + 1
-    return l
+groups = ['HOT', 'Seventeeen', 'Black Pink', 'NJZ']
+#ratings = [1, 2, 4, 3, 100]
+ratings = [1, 2, 4, 3]
 
-
-l = [99, 0, -7, 0, 16]
-move_zeros(l)
-print(l)
-
-
-
-#import array
-#
-#arr = array.array('f',[99, 0, -7, 0, 16])
-#
-#for i in range(len(arr)):
-#    print(f"{arr[i]:3}, {id(arr[i])}")
-
-
-
+group_rating = list(zip(groups, ratings))
+print(group_rating)
