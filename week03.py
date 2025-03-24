31c34f463e6c834bceb0e7059a39f8c6034fded1
plok108 <plok108@naver.com>
2025-03-24 17:58:28 +0900

    v0.9 array set = dict2

--- week03.py
+++ week03.py
@@ -1,6 +1,21 @@
-cities = ['Incheon', 'Seoul', 'Incheon', 'Incheon', 'Gwangju']
-cities = set(cities)
-cities.add('Incheon')
-cities.add('Suwon')
+def duplicate_city(cities):
+    result_city = []
+    s = set()
 
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2:
+            result_city.append(city)
+    return result_city
+
+cities = ['Incheon', 'Seoul', 'Incheon', 'Incheon', 'Gwangju']
+#cities = set(cities)
+#cities.add('Incheon')
+#cities.add('Suwon')
+cities.append('Incheon')
+cities.append('Seoul')
+cities.append('Suwon')
 print(cities)
+print(set(duplicate_city(cities)))  # 중복값을 찾아서 하나만 나오도록 하는 방법
