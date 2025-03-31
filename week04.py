9bead4c0ef249e9ea0048845e6c52cfb974449f6
plok108 <plok108@naver.com>
2025-03-31 17:19:24 +0900

    v1.4 linked list

--- week04.py
+++ week04.py
@@ -24,11 +24,10 @@ class LinkedList:
         current = self.head
         result = ""
         while current is not None:
-            # print(current.data)
-            result = result + str(current.data) + " -> "
+            # result = result + str(current.data) + " -> "
+            result = result + f"{current.data} -> "
             current = current.link
         return result + "END"
-        # return "Linked list!"
 
 
 ll = LinkedList()
