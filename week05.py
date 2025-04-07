dc287e8956e52cae834e0af97113b17a7154bd43
plok108 <plok108@naver.com>
2025-04-07 17:47:13 +0900

    v2.2 stack3

--- week05.py
+++ week05.py
@@ -7,7 +7,6 @@ class Stack:
     def __init__(self):
         self.top = None
 
-
     def push(self, data):
         node = Node(data)
         if self.top is None:
@@ -15,7 +14,6 @@ class Stack:
         else:
             node.link = self.top
             self.top = node
-
 
     def pop(self):
         if self.top is None:
@@ -25,10 +23,15 @@ class Stack:
         popped_node.link = None
         return popped_node.data
 
+    def peek(self):
+        return self.top.data
 
 s1 = Stack()
-print(s1.pop())
+#print(s1.pop())
 s1.push("Data structure")
 s1.push("Database")
-print(s1.pop())
-print(s1.pop())
+#print(s1.pop())
+s1.pop()
+print(s1.peek())
+#for i in range(2):
+#    print(s1.pop())
