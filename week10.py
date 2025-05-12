4f0adbb1843e4cc9bb270f2691eef5efd0281b43
plok108 <plok108@naver.com>
2025-05-12 16:54:34 +0900

    v3.7 tree search(ë³ë°(매개변수)

--- week10.py
+++ week10.py
@@ -30,8 +30,7 @@ def post_order(node):
     print(node.data, end="->")
 
 
-def search():
-    find_number = int(input("찾는 수는? "))
+def search(find_number):
     current = root
     while True:
         if find_number == current.data:
@@ -81,4 +80,5 @@ if __name__ == "__main__":
     print('BST 구성 완료')
     post_order(root)
     print()
-    search()
+    find_number = int(input("찾는 수는? "))
+    search(find_number)
