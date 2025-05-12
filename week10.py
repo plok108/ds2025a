7588461b98371d7101081e84d132a721cc80f755
plok108 <plok108@naver.com>
2025-05-12 18:06:57 +0900

    v4.0 tree delete(node 2)

--- week10.py
+++ week10.py
@@ -80,6 +80,18 @@ def delete(node, value):
             return node.right
         elif node.right is None:
             return node.left
+        # 자식이 2개인 노드를 삭제 (오른쪽에서 가장 작은 값)
+        # min_larger_node = node.right
+        # while min_larger_node.left:
+        #     min_larger_node = min_larger_node.left  # move(이동)
+        # node.data = min_larger_node.data
+        # node.right = delete(node.right, min_larger_node.data)
+        # 자식이 2개인 노드를 삭제 (왼쪽에서 가장 큰 값)
+        max_smaller_node = node.left
+        while max_smaller_node.right:
+            max_smaller_node = max_smaller_node.right
+        node.data = max_smaller_node.data
+        node.left = delete(node.left, max_smaller_node.data)
     return node
 
 if __name__ == "__main__":
@@ -96,11 +108,12 @@ if __name__ == "__main__":
     print()
     pre_order(root)
     print()
-    find_number = int(input("찾는 수는? "))
-    if search(find_number):
-        print(f"{find_number}을(를) 찾았습니다")
-    else:
-        print(f"{find_number}이(가) 존재하지 않습니다")
+
+    # find_number = int(input("찾는 수는? "))
+    # if search(find_number):
+    #     print(f"{find_number}을(를) 찾았습니다")
+    # else:
+    #     print(f"{find_number}이(가) 존재하지 않습니다")
 
     delete_number = int(input("제거할 숫자는? "))
     root = delete(root, delete_number)
