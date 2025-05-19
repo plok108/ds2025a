1b9fa5e17493724308322ddebf0865a7d7c6f4a1
plok108 <plok108@naver.com>
2025-05-19 16:49:24 +0900

    v4.2 graph

--- week11.py
+++ week11.py
@@ -1,10 +1,10 @@
 class Graph:
     def __init__(self, size):
         self.size = size
-        self.graph = [[0 for _ in range(size)] for _ in range(size)] #리스트 컴프리헨션 : 한 줄로 간결하게 리스트를 생성하는 방법
-
+        self.graph = [[0 for _ in range(size)] for _ in range(size)]
 
 G1 = Graph(4)
+G2 = Graph(4)
 
 # 0 == A, 1 == B, 2 == C, 3 == D
 G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
@@ -16,4 +16,14 @@ print("G1 무방향 그래프")
 for r in range(G1.size):
     for c in range(G1.size):
         print(G1.graph[r][c], end=' ')
+    print()
+
+# 0 == A, 1 == B, 2 == C, 3 == D
+G2.graph[0][1] = 1; G2.graph[0][2] = 1
+G2.graph[3][0] = 1; G2.graph[3][2] = 1
+
+print("G2 방향 그래프")
+for r in range(G2.size):
+    for c in range(G2.size):
+        print(G2.graph[r][c], end=' ')
     print()
