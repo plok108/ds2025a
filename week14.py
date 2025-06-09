df671f530a19c1a830e24ce64e7e254cae8382fd
plok108 <plok108@naver.com>
2025-06-09 17:04:28 +0900

    v5.4 boj 1929

--- week14.py
+++ week14.py
@@ -1,8 +1,10 @@
+import math
+
 def is_prime(n) -> bool:
   if n <= 1:
       return False
   else:
-    for i in range(2, n):
+    for i in (2, int(math.sqrt(n)) + 1):
         if n % i == 0:
           return False
   return True
@@ -11,11 +13,3 @@ s, e = map(int, input().split())
 for i in range(s, e+1):
     if is_prime(i):
         print(i)
-
-# inputs = input().split()
-# t = []
-# for i in inputs:
-#     t.append(int(i))
-# print(t)
-# s, e = t
-# print(s, e)
